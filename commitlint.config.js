81913968435c4ace2e66b0efca98f8bb5990e3df
Huzaima <khanhuzaima@gmail.com>
2022-01-21 03:21:29 +0500

    fix(RL-112): Hello

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,22 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+const supportedCase = [
+  "lower-case",
+  "upper-case",
+  "camel-case",
+  "kebab-case",
+  "pascal-case",
+  "sentence-case",
+  "snake-case",
+  "start-case",
+];
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-case": [2, "always", supportedCase],
+    // "header-case": [2, "always", supportedCase],
+    "scope-case": [2, "always", supportedCase],
+    "subject-case": [2, "always", supportedCase],
+    "type-case": [2, "always", "lower-case"],
+    "type-enum": [2, "always", ["feat", "fix"]],
+  },
+};
